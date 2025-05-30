b2c33972b2a443ec0a0b67725d36ffa697534cb8
LucaMariaPicuno <luca.picuno@gmail.com>
2025-05-30 21:52:31 +0200

    Update start.sh1

--- start.sh
+++ start.sh
@@ -2,12 +2,14 @@
 
 echo "⬇️ Scarico Chrome e Chromedriver..."
 
+# Scarica Chrome
 curl -sSL https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o chrome.deb
 ar x chrome.deb
 tar -xf data.tar.xz
 mv ./opt/google/chrome/chrome ./chrome
-chmod +x chrome  # <--- QUESTA RIGA È FONDAMENTALE
+chmod +x chrome  # QUESTA È LA RIGA CHIAVE
 
+# Scarica Chromedriver
 curl -sSL https://storage.googleapis.com/chrome-for-testing-public/120.0.6099.109/linux64/chromedriver-linux64.zip -o chromedriver.zip
 unzip chromedriver.zip
 mv chromedriver-linux64/chromedriver ./chromedriver
