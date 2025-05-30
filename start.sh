cab0da41b4fd63d1c3302eb5b7f3ca80dc5b8206
LucaMariaPicuno <luca.picuno@gmail.com>
2025-05-30 22:30:09 +0200

    Update start.sh543

--- start.sh
+++ start.sh
@@ -2,17 +2,19 @@
 
 echo "⬇️ Scarico Chrome e Chromedriver..."
 
-# Chrome
+# Scarica Chrome
 curl -sSL https://github.com/puppeteer/puppeteer/releases/latest/download/chrome-linux.zip -o chrome-linux.zip
 unzip -q chrome-linux.zip
 mv chrome-linux/* ./
-chmod +x /app/chrome    # ✅ Rende chrome eseguibile
 
-# Chromedriver
+# Rendi Chrome eseguibile
+chmod +x /app/chrome
+
+# Scarica Chromedriver
 curl -sSL https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.111/linux64/chromedriver-linux64.zip -o chromedriver.zip
 unzip -q chromedriver.zip
 mv chromedriver-linux64/chromedriver ./chromedriver
 chmod +x /app/chromedriver
 
-# Avvio server Flask
+# Avvia il server
 gunicorn main:app --bind 0.0.0.0:$PORT
