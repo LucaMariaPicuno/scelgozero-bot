23c0fc2fa8302909c89e929cca1f2941b42dca9d
LucaMariaPicuno <luca.picuno@gmail.com>
2025-05-30 23:17:52 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,16 +5,16 @@ echo "⬇️ Scarico Chrome e Chromedriver..."
 # Crea directory
 mkdir -p /tmp/chrome /tmp/chromedriver
 
-# Scarica Chrome
+# Scarica ed estrai Chrome
 curl -sSL https://github.com/puppeteer/puppeteer/releases/latest/download/chrome-linux.zip -o chrome-linux.zip
 unzip -q chrome-linux.zip -d /tmp/chrome
 chmod +x /tmp/chrome/chrome
 
-# Scarica Chromedriver
+# Scarica ed estrai Chromedriver
 curl -sSL https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.111/linux64/chromedriver-linux64.zip -o chromedriver.zip
-unzip -q chromedriver.zip -d /tmp/chromedriver
-mv /tmp/chromedriver/chromedriver-linux64/chromedriver /tmp/chromedriver/chromedriver
+unzip -q chromedriver.zip -d /tmp/
+mv /tmp/chromedriver-linux64/chromedriver /tmp/chromedriver/chromedriver
 chmod +x /tmp/chromedriver/chromedriver
 
-# Avvia Gunicorn
+# Avvia il server
 gunicorn main:app --bind 0.0.0.0:$PORT
