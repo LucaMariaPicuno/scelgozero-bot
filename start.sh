3f95e639f8afcfc02ebc930e701b33c4a0e9ead8
LucaMariaPicuno <luca.picuno@gmail.com>
2025-05-30 22:48:26 +0200

    Update start.shrew

--- start.sh
+++ start.sh
@@ -5,8 +5,8 @@ echo "⬇️ Scarico Chrome e Chromedriver..."
 # Scarica Chrome
 curl -sSL https://github.com/puppeteer/puppeteer/releases/latest/download/chrome-linux.zip -o chrome-linux.zip
 unzip -q chrome-linux.zip
-cp chrome-linux/chrome /app/chrome
-chmod +x /app/chrome
+mv chrome-linux/chrome /app/chrome
+chmod 755 /app/chrome
 
 # Scarica Chromedriver
 curl -sSL https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.111/linux64/chromedriver-linux64.zip -o chromedriver.zip
