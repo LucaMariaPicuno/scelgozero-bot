fc0d05d13cddd1ec4f2cf5adbb0da790884a6e58
LucaMariaPicuno <luca.picuno@gmail.com>
2025-05-30 22:11:15 +0200

    Update start.sh4

--- start.sh
+++ start.sh
@@ -5,20 +5,13 @@ echo "⬇️ Scarico Chrome e Chromedriver..."
 # Scarica Chrome
 curl -sSL https://storage.googleapis.com/chrome-for-testing-public/120.0.6099.109/linux64/chrome-linux64.zip -o chrome.zip
 unzip -o chrome.zip
-mv -f chrome-linux64/chrome ./chrome
-chmod 755 ./chrome
-echo "✔️ Chrome reso eseguibile"
 
 # Scarica Chromedriver
 curl -sSL https://storage.googleapis.com/chrome-for-testing-public/120.0.6099.109/linux64/chromedriver-linux64.zip -o chromedriver.zip
 unzip -o chromedriver.zip
-mv -f chromedriver-linux64/chromedriver ./chromedriver
-chmod 755 ./chromedriver
-echo "✔️ Chromedriver reso eseguibile"
 
-# Verifica permessi
-ls -l ./chrome
-ls -l ./chromedriver
+mv -f chromedriver-linux64/chromedriver ./chromedriver
+chmod +x ./chromedriver
 
 # Avvia il server Flask
 exec gunicorn -b 0.0.0.0:8080 main:app
